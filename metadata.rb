f90aa872a0b30e5168dac8b2598c131015f252b8
Niklas Hofer <niklas+dev@lanpartei.de>
2010-07-22 13:56:04 +0200

    generated lxc cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Company"
+maintainer_email "ssl@example.com"
+license          "Apache 2.0"
+description      "Installs/Configures lxc"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
